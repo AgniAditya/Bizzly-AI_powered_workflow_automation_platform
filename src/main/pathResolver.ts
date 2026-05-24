8ed4a667f63d5b46dc06f4e073c5ae0ab4efc552
AgniAditya <agniaditya05@gmail.com>
2026-05-25 01:53:13 +0530

    feat: update build configuration and preload path handling for improved resource management

--- src/main/pathResolver.ts
+++ src/main/pathResolver.ts
@@ -3,6 +3,9 @@ import { app } from 'electron';
 import { isDevMode } from './util.js';
 
 export function getPreloadPath(): string {
-    return path.join(app.getAppPath(), 
-    isDevMode() ? '.' : '..', '/dist-electron/preload.cjs');
+    if (isDevMode()) {
+        return path.join(app.getAppPath(), "dist-electron", "preload.cjs");
+    }
+
+    return path.join(process.resourcesPath, "dist-electron", "preload.cjs");
 }
